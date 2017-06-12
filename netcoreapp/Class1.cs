93906893e22b815bbfa1cfe0c001d115518d0527
Patrick Boisen <patrick@skarp.dk>
2017-06-12 20:11:23 +0200

    Add netcoreapp and netstandard projects
    Only difference is the target framework, but the "netcoreapp" project compiles - the other does not.

---
+++ netcoreapp/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netcoreapp
+{
+    public class Class1
+    {
+	public void Method()
+	{
+		Console.WriteLine("Hello world".Normalize());
+	}
+    }
+}
---
+++ netstandard/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netstandard
+{
+    public class Class1
+    {
+	public void Method()
+	{
+		Console.WriteLine("Hello world".Normalize());
+	}
+    }
+}
